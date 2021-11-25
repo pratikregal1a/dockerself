cd32c403b2788626faac51118370e34fe41333d7
pratikregal1a <57472214+pratikregal1a@users.noreply.github.com>
2021-11-25 14:53:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,18 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def homepage():
-    if request.method == "GET":
-        return jsonify({"message": "Hello World!"})
+@app.route("/")
+def hello():
+    html = "<h2>HTML Images</h2>"
+            "<p>HTML images are defined with the img tag:</p>"
+
+            "<img src="w3schools.jpg" alt="W3Schools.com" width="104" height="142">"
+    return html
+
+# @app.route("/", methods=["GET"])
+# def homepage():
+#     if request.method == "GET":
+#         return jsonify({"message": "Hello World!"})
 
 PORT = int(os.environ.get("PORT", 8080))
 if __name__ == '__main__':
